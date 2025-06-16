37518df98f2915cd7fbd00a96795a2c0a385c4c5
RZ-Aniket <aniket.sharma@rezolut.in>
2025-06-17 00:57:31 +0530

    message read and consumer done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { KafkaModule } from './kafka/kafka.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [KafkaModule],
+  imports: [
+     ConfigModule.forRoot({
+      isGlobal: true,
+    })
+    ,KafkaModule,DatabaseModule,MessageModule],
   controllers: [],
   providers: [],
 })
